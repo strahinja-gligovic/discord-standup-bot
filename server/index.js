3ca39788d83be4a60d849a05713b876c281831b4
Strahinja-DESKTOP <strahinja.gligovic@gmail.com>
2020-08-06 21:37:24 +0200

    setup ejs

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
 function initializeServer() {
+    app.set('view engine', 'ejs');
+    app.set('views', path.join(__dirname, '/views'));
+
     app.get('/', (req, res) => {
-        res.send('Hello World!')
+        res.render('index');
     })
+
 
     app.listen(port, () => {
         console.log(`Example app listening at http://localhost:${port}`)
